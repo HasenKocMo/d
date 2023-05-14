1e31d0f3481831860ae68cb1319bdab9db8c6bc7
frorruchi <102749125+frorruchi@users.noreply.github.com>
2023-05-14 20:27:23 +0300

    Update 1 script.js
    start

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 
+function getRandomInt(min, max) {
+     min = Math.ceil(min);
+     max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; 
+}
+
+
+let cards = []
+//let eliminated = [] maybe
+
+// где то тут splt и в список/ запушить проклятия
+
+class Card{
+    constructor() {
+        this.text = this.text = cards[this.cardid]
+
+        this.cardid = getRandomInt(1,cards.length)
+
+        display() {
+            // здесь вывод текста на карту
+            // eliminated.push(cards[this.cardid]) maybe
+            delete cards[this.cardid]
+            }
+    }                    
+}
+
+
+
+var card = document.querySelector(".card");
+var playing = false;
+
+card.addEventListener('click',function() {
+  if(playing)
+    return;
+  
+  playing = true;
+  anime({
+    targets: card,
+    scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
+    rotateY: {value: '+=180', delay: 200},
+    easing: 'easeInOutSine',
+    duration: 600,
+    complete: function(anim){
+       playing = false;
+    }
+  });
+});
